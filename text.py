464af2669df4d52d2790482bd9221118c6ac48e6
김충만 <cndaks1269@naver.com>
2025-02-12 12:26:59 +0900

    3.6.9일때 함수

--- text.py
+++ text.py
@@ -8,6 +8,14 @@ True라면 계속 진행을 False라면 문구와 함깨 종료한다.
 # 사용자 입력 함수 만들기
 def user_input():
     return input("사용자 차례: ")
-# 컴퓨터 입력 만들기
+# 3.6.9 일때 함수 만들기
+def check_369(number):
+    number = str(number)
+    clap_count = 0
+    for i in range(len(number)):
+        if number[i] == "3" or number[i] == "6" or number[i] == "9":
+            clap_count += 1
+    return clap_count
+
 # 100까지 숫자열 만들기
 # True, False 판별하기
